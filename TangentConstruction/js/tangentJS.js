5391fee3c3c55e3c7ad8c9c3b73737743c1d884d
Kyle Genter <omishrakefight@live.com>
2018-12-26 17:43:57 -0500

    added JavaScript footer
    added footer and custom Javascript, footer information is automatically added to each page and saved in one spot.

---
+++ TangentConstruction/js/tangentJS.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+
+
+  $('.FooterInfo').append('p').text('I add here')
+  //This is my footer
+  $('Footer').append('p').html(
+    '<div class="col-md-12">' +
+    '<br>' +
+    '<hr style="border-top: 1px solid #333">' +
+    '<p style="text-align: center"> Tangent Construction Services, LLC  &nbsp &nbsp &nbsp | ' +
+    ' &nbsp &nbsp &nbsp (813) 628-4411  &nbsp &nbsp &nbsp |' +
+    ' &nbsp &nbsp &nbsp info@tangentconstructionservices.com ' +
+    '<br>' +
+    '<br>' +
+    '</p>' +
+    '</div>'
+  )
+
+});
